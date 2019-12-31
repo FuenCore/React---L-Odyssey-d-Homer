cacecdc06fb378fc8743fedf04539314dac5af2b
FuenCore <fuentes33160@gmail.com>
2019-12-31 20:16:38 +0100

    quest020 done

--- front/odyssey/src/components/SignUp.js
+++ front/odyssey/src/components/SignUp.js
@@ -2,15 +2,30 @@ import React, {useState} from 'react';
 
 const SignUp = (props) => {
     const [form, setForm] = useState({
-        email: ""
+        firstname: "",
+        lastname: "",
+        email: "",
+        password: ""
+  
     });
     const updateInput = (event) => {
         setForm({ ...form, [event.target.name]: event.target.value })
 };
+    const submitInput = (event) => {
+        event.preventDefault();	    
+        console.log(form);
+    }
     return(
 <div>
-    <h1>{form.email}</h1>
+    <h1>{JSON.stringify(form, 1, 1)}</h1>
+    <form onSubmit={submitInput}>
+    <input type="text" name="firstname" placeholder="firstname" onChange={updateInput} /> 
+    <input type="text" name="lastname" placeholder="lastname" onChange={updateInput} /> 
     <input type="email" name="email" placeholder="email" onChange={updateInput}/>
+    <input type="password" name="password" placeholder="password" onChange={updateInput} /> 
+    <input type="password" name="passwordconf" placeholder="confirm password" onChange={updateInput} /> 
+    <input type="submit" value="Submit"/>
+    </form>
 </div>
     );
 }
